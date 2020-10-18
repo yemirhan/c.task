23dae6ab1e36fc6ca259052b77c0434eb3dce9b5
Doguhan Okumus <douhan@gmail.com>
2020-10-19 02:26:08 +0300

    base

--- src/App.js
+++ src/App.js
@@ -1,13 +1,65 @@
 import React from 'react';
-
+import _ from 'lodash';
+import './App.css';
+import 'antd/dist/antd.css';
+import { Layout, Menu, Breadcrumb } from 'antd';
 import Elements from './components/elements';
+
+const { Header, Content, Footer, Sider } = Layout;
 const { Chart } = Elements;
 
 function App() {
+  const [data, set_data] = React.useState([])
+  const [element, setElement] = React.useState(0);
+
+  const elements = [
+    {
+      title: 'Chart for Values',
+      element: <Chart data={data} />
+    },
+    null
+  ];
+
+
+  React.useEffect(() => {
+    set_data(_.chain(0).range(100).map((v, k) => ({ x: k, y: v * v * v })).value())
+  }, [])
+
+
   return (
-    <div>
-      <Chart />
-    </div>
+    <Layout>
+      <Header className="header">
+        <div className="logo" />
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+          <Breadcrumb.Item>Elements</Breadcrumb.Item>
+        </Breadcrumb>
+        <Layout className="site-layout-background" style={{ padding: '24px 0' }}>
+          <Sider className="site-layout-background" width={200}>
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={['1']}
+              defaultOpenKeys={['sub1']}
+              style={{ height: '100%' }}
+            >
+              {_.map(elements, (e, k) => <Menu.Item onClick={() => setElement(k)} key={k}>{e?.title || 'Your Element'}</Menu.Item>)}
+            </Menu>
+          </Sider>
+          <Content style={{ padding: '0 24px', minHeight: 280 }}>
+
+            {elements[element]?.element ? <div class="content-card">
+              <div class="content-title">{elements[element].title}</div>
+              {elements[element].element}
+            </div> : <div>Design your component and add to elements[] in App.js.</div>}
+
+          </Content>
+        </Layout>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
   );
 }
 
--- src/components/elements/Chart.js
+++ src/components/elements/Chart.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+am4core.useTheme(am4themes_animated);
 
 export default function Chart({ data = [] }) {
+    const chart = React.useRef(null);
 
-    return <div>
-        Chart
-    </div>;
+    const prepare = () => {
+
+        chart.current = am4core.create("chartdiv", am4charts.XYChart)
+        chart.current.data = data;
+
+        let dateAxis = chart.current.xAxes.push(new am4charts.ValueAxis());
+        dateAxis.renderer.minGridDistance = 30;
+
+        let valueAxis = chart.current.yAxes.push(new am4charts.ValueAxis());
+
+        // Create series
+        let series = chart.current.series.push(new am4charts.LineSeries());
+        series.dataFields.valueY = "y";
+        series.dataFields.valueX = "x";
+        series.tooltipText = "{y}"
+
+        series.tooltip.pointerOrientation = "vertical";
+
+        chart.current.cursor = new am4charts.XYCursor();
+        chart.current.cursor.snapToSeries = series;
+        chart.current.cursor.xAxis = dateAxis;
+
+        // chart.current.scrollbarY = new am4core.Scrollbar();
+        // chart.current.scrollbarX = new am4core.Scrollbar();
+    }
+
+    React.useEffect(() => {
+        prepare();
+    }, [data])
+
+    return <div style={{ width: '100%', height: '100%' }} >
+        <div id='chartdiv' style={{width: '100%', height: '100%'}}></div>
+    </div>
 
 }
